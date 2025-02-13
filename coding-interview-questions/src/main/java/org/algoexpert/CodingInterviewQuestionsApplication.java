89127ea313bd72d7bf8df610c6113b57312e2d43
jiteshshaw93 <jiteshshaw93@gmail.com>
2025-02-13 18:46:35 +0530

    renamed main class

--- coding-interview-questions/src/main/java/org/algoexpert/CodingInterviewQuestionsApplication.java
+++ coding-interview-questions/src/main/java/org/algoexpert/CodingInterviewQuestionsApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AlgoExpertApplication {
+public class CodingInterviewQuestionsApplication {
     public static void main(String[] args) {
-        SpringApplication.run(AlgoExpertApplication.class, args);
+        SpringApplication.run(CodingInterviewQuestionsApplication.class, args);
     }
 }
